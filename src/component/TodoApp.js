2ee248494aaa6e58882b812dd954ff022e82e845
McWooden <garryf7887@gmail.com>
2022-11-27 00:26:52 +0700

    sabtu update last

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -6,6 +6,8 @@ export class TodoApp extends React.Component {
         return (
             <div id='todoApp'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perspiciatis repellendus, laborum laboriosam expedita laudantium incidunt accusantium illum est porro earum quo at? Sint quidem ipsa illum accusantium porro debitis!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam voluptates harum, asperiores soluta error, iste voluptatem non commodi, molestias repellat cum saepe laborum deleniti. Repudiandae dolorum aperiam pariatur consequatur.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
             </div>
         )
     }
