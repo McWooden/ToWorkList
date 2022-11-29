f794febf5f5d0992ad2faf5bd58086d5ba8b3140
McWooden <garryf7887@gmail.com>
2022-11-29 14:55:59 +0700

    commit selasa 29

--- src/component/Base.js
+++ src/component/Base.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faBars} from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/free-solid-svg-icons'
 import './style/base.css'
 
 export class Base extends Component {
-  render() {
-    return (
-    <div className='base'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perspiciatis repellendus, laborum laboriosam expedita laudantium incidunt accusantium illum est porro earum quo at? Sint quidem ipsa illum accusantium porro debitis!</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam voluptates harum, asperiores soluta error, iste voluptatem non commodi, molestias repellat cum saepe laborum deleniti. Repudiandae dolorum aperiam pariatur consequatur.</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
-    </div>
-    )
-  }
+    render() {
+        return (
+            <div className='base'>
+                <BaseLeft/>
+                <BaseCenter/>
+                <BaseRight/>
+            </div>
+        )
+    }
+}
+class BaseLeft extends Component {
+    render() {
+        return (
+            <div className="base-left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+            </div>
+        )
+    }
+}
+class BaseCenter extends Component {
+    render() {
+        return (
+            <div className="base-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perspiciatis repellendus, laborum laboriosam expedita laudantium incidunt accusantium illum est porro earum quo at? Sint quidem ipsa illum accusantium porro debitis!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam voluptates harum, asperiores soluta error, iste voluptatem non commodi, molestias repellat cum saepe laborum deleniti. Repudiandae dolorum aperiam pariatur consequatur.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+            </div>
+        )
+    }
+}
+class BaseRight extends Component {
+    render() {
+        return (
+            <div className="base-right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem doloremque non tenetur? Tenetur, velit id nesciunt, quibusdam sit perferendis qui, fuga modi expedita nam deleniti at nihil ad corporis.</p>
+            </div>
+        )
+    }
 }
 
 export default Base
