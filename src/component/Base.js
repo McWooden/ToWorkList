b2c0f0c4654f35086e5bd9194a4ece6a5495be9b
McWooden <garryf7887@gmail.com>
2022-11-30 18:55:21 +0700

    sekarang kalo scroll navbar ngak nge scroll body, karna todoapp nya ngak di body

--- src/component/Base.js
+++ src/component/Base.js
@@ -7,9 +7,9 @@ export class Base extends Component {
     render() {
         return (
             <div className='base'>
-                <BaseLeft/>
-                <BaseCenter/>
-                <BaseRight/>
+                    <BaseLeft/>
+                    <BaseCenter/>
+                    <BaseRight/>
             </div>
         )
     }
@@ -18,7 +18,26 @@ class BaseLeft extends Component {
     render() {
         return (
             <div className="base-left">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                <div className="sidebar-right">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem optio magni hic quibusdam, cupiditate numquam ducimus ullam vitae aut nobis aliquid doloribus ipsa id eos enim reiciendis saepe? Earum!</p>
+                </div>
             </div>
         )
     }
@@ -27,9 +46,41 @@ class BaseCenter extends Component {
     render() {
         return (
             <div className="base-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perspiciatis repellendus, laborum laboriosam expedita laudantium incidunt accusantium illum est porro earum quo at? Sint quidem ipsa illum accusantium porro debitis!</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam voluptates harum, asperiores soluta error, iste voluptatem non commodi, molestias repellat cum saepe laborum deleniti. Repudiandae dolorum aperiam pariatur consequatur.</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                <div className="center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae perspiciatis repellendus, laborum laboriosam expedita laudantium incidunt accusantium illum est porro earum quo at? Sint quidem ipsa illum accusantium porro debitis!</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ullam voluptates harum, asperiores soluta error, iste voluptatem non commodi, molestias repellat cum saepe laborum deleniti. Repudiandae dolorum aperiam pariatur consequatur.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus accusantium commodi quasi, laboriosam animi iusto repudiandae. Possimus veniam iusto magnam porro eum cumque adipisci quod, fuga non voluptatum velit. Nisi?</p>
+                </div>
             </div>
         )
     }
@@ -38,8 +89,20 @@ class BaseRight extends Component {
     render() {
         return (
             <div className="base-right">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem doloremque non tenetur? Tenetur, velit id nesciunt, quibusdam sit perferendis qui, fuga modi expedita nam deleniti at nihil ad corporis.</p>
+                <div className="sidebar-right">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatem doloremque non tenetur? Tenetur, velit id nesciunt, quibusdam sit perferendis qui, fuga modi expedita nam deleniti at nihil ad corporis.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor consectetur, provident, optio voluptatem architecto nam maiores harum quos beatae odit dicta assumenda ipsa accusantium molestiae nihil at nesciunt repellat ea!</p>
+                </div>
             </div>
         )
     }
