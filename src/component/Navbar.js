fa194970da148a62f99f8c68ba991bb7c8bab4d2
McWooden <garryf7887@gmail.com>
2023-02-26 21:52:51 +0700

    add server ui

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import './style/bookCard.css'
 import { AppContext, PageContext } from '../pages/App'
 import { useContext, useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faHouse, faGear, faPlus, faCompass, faRepeat, faSearch} from '@fortawesome/free-solid-svg-icons'
+import {faHouse, faGear, faPlus, faCompass, faRepeat, faSearch, faImage, faXmark} from '@fortawesome/free-solid-svg-icons'
 import * as fontawesome from '@fortawesome/free-solid-svg-icons'
 import { myAccount } from '../utils/dataJSON'
 import { GuildSetting } from './setting'
@@ -14,7 +14,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setFetch, setPathBook, setPathPageOfBook } from '../redux/fetchSlice'
 import { setBooksProfile, setGuildProfile, setMembers } from '../redux/sourceSlice'
 import { setPageType, setSource } from '../redux/sourceSlice'
-import { ModalLight } from './Modal'
+import { ModalLight, ModalSecond } from './Modal'
 
 const API = process.env.REACT_APP_API
 
@@ -129,18 +129,87 @@ function ModeNavbar() {
     )
 }
 function FindAndCreateBook() {
+    const myAccount = useSelector(state => state.source.profile)
+    const [addServerModal, setAddServerModal] = useState(false)
     const [modalOpen, setModalOpen] = useState(false)
     const [searchText, setSearchText] = useState('')
 
     function handleInputChange(e) {
         setSearchText(e.target.value)
     }
-    function handleSubmit(e) {
+    function handleSearch(e) {
         e.preventDefault()
         console.log(searchText)
     }
     function handleModalOpen() { setModalOpen(true) }
     function handleModalClose() { setModalOpen(false) }
+    // file drop
+    const fileInput = useRef(null)
+    const[image, setImage] = useState(null)
+    const[previewUrl, setPreviewUrl] = useState('')
+    const handleFile = file => {
+        setImage(file)
+        setPreviewUrl(URL.createObjectURL(file))
+    }
+
+    const handleOndragOver = event => {
+        event.preventDefault()
+    }
+    const handleOndrop = event => {
+        event.preventDefault()
+        event.stopPropagation()
+        let imageFile = event.dataTransfer.files[0]
+        handleFile(imageFile)
+    }
+    const handleRemove = () => {
+        formRef.current.reset()
+        setImage(null)
+        setPreviewUrl('')
+    }
+    // form
+    const formRef = useRef()
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('image', image)
+        formData.append('data', {})
+        try {
+            startInterval()
+            setBtnLoading(true)
+            await axios.post(`${API}/image/book`, formData)
+            .then(res => {
+                setModalOpen(false)
+                formRef.current.reset()
+                setImage(null)
+                setPreviewUrl('')
+            })
+            .catch(err => {
+
+            }).finally(() => {
+                stopInterval()
+                setBtnLoading(false)
+            })
+        } catch(err) {
+            setBtnLoading(false)
+            stopInterval()
+        }
+    }
+    const [intervalId, setIntervalId] = useState(null)
+    const [count, setCount] = useState(0)
+    const startInterval = () => {
+        const intervalId = setInterval(() => {
+            setCount((count) => count + 1)
+        }, 1000)
+        setIntervalId(intervalId)
+    }
+    const stopInterval = () => {
+        console.log(intervalId)
+        clearInterval(intervalId)
+        setCount(0)
+        setIntervalId(null)
+    }
+    const [btnLoading, setBtnLoading] = useState(false)
+    const [valueJudul, setValueJudul] = useState(`Buku ${myAccount.nickname}`)
     return (
         <>
         <div className='find-create-frame'>
@@ -151,7 +220,7 @@ function FindAndCreateBook() {
         <ModalLight open={modalOpen} close={handleModalClose}>
             <div className="search_book_container">
                 <div className="search_book-header">
-                    <form onSubmit={handleSubmit} className='form-modal'>
+                    <form onSubmit={handleSearch} className='form-modal'>
                         <div className='search_bar'>
                             <input type="text" value={searchText} onChange={handleInputChange} placeholder="Fitur belum dibuka" />
                             <div className="tiang"/>
@@ -161,12 +230,50 @@ function FindAndCreateBook() {
                         </div>
                     </form>
                     <div className="sb_action_btn">
-                            <FontAwesomeIcon icon={faPlus} className='action_btn'/>
+                        <FontAwesomeIcon icon={faPlus} className='action_btn' onClick={() => setAddServerModal(true)}/>
                     </div>
                 </div>
                 <AllBookList/>
             </div>
         </ModalLight>
+        <ModalSecond open={addServerModal} close={() => setAddServerModal(false)}>
+            <form className="add_server_container form-modal" ref={formRef} onDragOver={handleOndragOver} onDrop={handleOndrop} onSubmit={handleSubmit}>
+                <div className="add_server-body">
+                    <h3>Membuat Buku</h3>
+                    <p>Beri buku baru anda nama dan pp. Anda juga bisa menggantinya kapan saja nanti</p>
+                    <div className="pp_buku">
+                        <div className="img-view" onClick = { () => {try{fileInput.current.click()} catch(err){}}}>
+                            { previewUrl ? 
+                                <>
+                                <img src={previewUrl} alt={image.name} /> 
+                                <FontAwesomeIcon icon={faXmark} className='remove-icon' onClick={handleRemove}/>
+                                </>
+                            :
+                                <div className="drop-zone">
+                                    <FontAwesomeIcon icon={faImage} className='drop-icon'/>
+                                    <input 
+                                        type="file" 
+                                        accept='image/*' 
+                                        ref={fileInput} hidden 
+                                        onChange={e => handleFile(e.target.files[0])}
+                                    />
+                                </div>
+                            }
+                        </div>
+                    </div>
+                    <h4>Judul Buku</h4>
+                    <input type="text" value={valueJudul} placeholder='Judul' onChange={(e) => setValueJudul(e.target.value)}/>
+                </div>
+                <div className="add_server-footer addPage_action">
+                    <span className='btn_action' onClick={() => setAddServerModal(false)}>Batal</span>
+                    {btnLoading?
+                    (<button className={`btn_action btn_add`}>Loading...{count}</button>)
+                    :
+                    (<button type='submit' className={`btn_action btn_add ${valueJudul&&'active'}`}>Buat</button>)
+                    }
+                </div>
+            </form>
+        </ModalSecond>
         </>
     )
 }
