3cea4432c87de49d826d72db6fc1184003f4f9f0
McWooden <garryf7887@gmail.com>
2023-07-12 16:22:59 +0700

    no default tailwind

--- src/component/Page/summary.js
+++ src/component/Page/summary.js
@@ -11,6 +11,7 @@ import { setProfile } from '../../redux/sourceSlice'
 import { setLocalAccount } from '../../utils/localstorage'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import remarkGfm from 'remark-gfm'
+import emoji from 'remark-emoji'
 
 
 export default function Summary() {
@@ -117,12 +118,12 @@ export default function Summary() {
             </div>
             <div className="mt-2.5 bg-zinc-800 whitespace-pre rounded h-fit p-1 max-w-full">
                 <div className='overflow-y-auto text-sm'>
-                    <ReactMarkdown remarkPlugins={[remarkGfm]}>{myProfile?.bio}</ReactMarkdown>
+                    <ReactMarkdown remarkPlugins={[remarkGfm, emoji]}>{myProfile?.bio}</ReactMarkdown>
                 </div>
             </div>
         </div>
         <ModalSecond open={modalProfileEditForm} close={() => setModalProfileEditForm(false)}>
-            <form onSubmit={handleSubmitProfileForm} className="m-auto max-h-[90vh] text-xs p-5 sm:p-10 max-w-xl h-screen of-auto" id="profileEditForm">
+            <form onSubmit={handleSubmitProfileForm} className="m-auto max-h-[90vh] text-xs p-5 sm:p-10 d-flex flex-col max-w-xl h-screen of-auto" id="profileEditForm">
             <label htmlFor="nickname" className="block text-sm font-semibold leading-6 text-stone-100">Nama</label>
             <input onChange={(e) => setInputNickname(e.target.value)}  value={inputNickname} id='nickname' type="text" placeholder='Nama' className='outline-none block w-full rounded-md border-0 px-3.5 py-2 text-zinc-200 shadow-sm ring-1 ring-inset ring-zinc-600 bg-zinc-700 placeholder:text-stone-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'/>
             <label htmlFor="panggilan" className="block mt-2.5 text-sm font-semibold leading-6 text-stone-100">Panggilan</label>
@@ -191,10 +192,10 @@ export default function Summary() {
                     </div>
                 </div>
             </div>
-            <div className="sm:col-span-2">
-                <div className="mt-2.5">
-                <label htmlFor="bio" className="block text-sm font-semibold leading-6 text-stone-100">Bio</label>
-                <textarea onChange={(e) => setInputBio(e.target.value)}  value={inputBio} name="bio" id="bio" rows="4" placeholder='Bio' className="overflow-y-auto whitespace-pre outline-none block w-full rounded-md border-0 px-3.5 py-2 text-zinc-200 bg-zinc-700 shadow-sm ring-1 ring-inset ring-zinc-600 placeholder:text-stone-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            <div>
+                <div className="my-2.5 flex-1">
+                    <label htmlFor="bio" className="block text-sm font-semibold leading-6 text-stone-100">Bio</label>
+                    <textarea onChange={(e) => setInputBio(e.target.value)}  value={inputBio} name="bio" id="bio" rows="4" placeholder='Bio' className="overflow-y-auto mb-2.5 h-full whitespace-pre outline-none block w-full rounded-md border-0 px-3.5 py-2 text-zinc-200 bg-zinc-700 shadow-sm ring-1 ring-inset ring-zinc-600 placeholder:text-stone-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                 </div>
             </div>
             <button type="submit" form="profileEditForm" className="mt-2.5 block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Simpan</button>
