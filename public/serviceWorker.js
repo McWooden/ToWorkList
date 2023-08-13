87c1a635a07164b274029fac0eb5f8ade77c9b52
McWooden <garryf7887@gmail.com>
2023-08-13 20:14:16 +0700

    beta testing pwa

---
+++ public/serviceWorker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'version-1'
+const urlsToChache = ['index.html', 'offline.html']
+
+const self = this
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            console.log('opened cache');
+            return cache.addAll(urlsToChache)
+        })
+    )
+})
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => fetch(event.request))
+            .catch(() => caches.match('offline.html'))
+    )
+})
+self.addEventListener('activate', (event) => {
+    const cachewhitelist = []
+    cachewhitelist.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            Promise.all(cacheNames.map(cacheName => !cachewhitelist.includes(cacheName) && caches.delete(cacheName)))
+        })
+    )
+})
