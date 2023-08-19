3963b862ac0a9de49bb5b26b7a1a59a4ffbf4d3b
McWooden <garryf7887@gmail.com>
2023-08-19 12:10:26 +0700

    mindahin sw load ke html

--- src/index.js
+++ src/index.js
@@ -24,17 +24,17 @@ import './styles/pages/App.css'
 import './styles/theme/theme.css'
 import './styles/welcome.css'
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/serviceworker.js")
-      .then((registration) => {
-        console.log("Service Worker registered with scope:", registration.scope)
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error)
-      })
-  })
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker.register("/serviceworker.js")
+//       .then((registration) => {
+//         console.log("Service Worker registered with scope:", registration.scope)
+//       })
+//       .catch((error) => {
+//         console.error("Service Worker registration failed:", error)
+//       })
+//   })
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
