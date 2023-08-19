04a1a06cc8afc9ac502f02684ae066e67ff1df71
McWooden <garryf7887@gmail.com>
2023-08-19 13:05:49 +0700

    coba pake crea pwa template

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { Provider } from 'react-redux'
 import Join from './pages/Join'
 import { AddAndEditForGlobal } from './component/Modal/addAndEditForGlobal'
 import { Flip } from 'react-toastify'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 
 // import all css
@@ -67,6 +69,11 @@ root.render(
   {/* </React.StrictMode> */}
   </>
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
